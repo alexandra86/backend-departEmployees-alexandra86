2400801091e149c6b96f25a12739bc34d7a49fe3
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-08-03 22:04:26 -0300

    feat: instalado e configurado o CORS para integrar o back com o front

--- employeersAndDepart/config/cors.php
+++ employeersAndDepart/config/cors.php
@@ -16,19 +16,12 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => ['http://localhost:5174'], 
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => ['Authorization', 'Content-Type', 'X-Requested-With'],
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
